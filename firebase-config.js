7f8d280c7ce4e990847321e95b7e17139d7dc7bf
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-12-30 19:06:55 +0300

    Add Firebase config and live countdown timer

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+w<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCm_13L3lVG_wtvNOJXRipCQGPUVtg54NE",
+    authDomain: "login-example-dbb13.firebaseapp.com",
+    projectId: "login-example-dbb13",
+    storageBucket: "login-example-dbb13.firebasestorage.app",
+    messagingSenderId: "111041206024",
+    appId: "1:111041206024:web:3a9bdc5ac126256638bebd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
